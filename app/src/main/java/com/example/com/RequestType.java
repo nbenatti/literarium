5ea416cf9e1c8c601a71445b2ad50f38d68fec66
FrankDawn <47036534+FrankDawn@users.noreply.github.com>
2019-05-11 12:55:11 +0200

    classes

---
+++ app/src/main/java/com/example/com/RequestType.java
@@ -0,0 +1,11 @@
+package literarium.parsingData;
+
+public enum RequestType {
+    AUTHOR_SHOW,
+    BOOK_SHOW,
+    AUTHOR_BOOKS,
+    SEARCH_BOOKS,
+    SERIES_LIST,
+    SERIES_SHOW,
+    SERIES_WORK;
+}
