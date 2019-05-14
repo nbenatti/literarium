64eb9b3a5d20ef64a9451d9ba2c6d8d230419f64
cinesikris <49337086+cinesikris@users.noreply.github.com>
2019-05-14 10:27:00 +0200

    Add files via upload

--- app/src/main/java/com/example/com/localDB/BookDAO.java
+++ app/src/main/java/com/example/com/localDB/BookDAO.java
@@ -1,11 +1,10 @@
-package com.example.com.localDB;
+package app.literarium.localDB;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
-
 import java.util.List;
 
 @Dao
@@ -16,11 +15,13 @@ public interface BookDAO {
     public void update(Book... books);
     @Delete
     public void delete(Book b);
-
+    
     @Query("SELECT * FROM book WHERE userId = :userId")
     public List<Book> getAllBooks(String userId);
     @Query("SELECT * FROM book WHERE status = 1 AND userId = :userId")
-    public List<Book> getSavedBooks(String userId);
+    public List<Book> getAllSavedBooks(String userId);
     @Query("SELECT * FROM book WHERE status = 0 AND userId = :userId")
-    public List<Book> getSentBooks(String userId);
+    public List<Book> getAllSentBooks(String userId);
+    @Query("SELECT * FROM book WHERE status = 1 AND userId = :userId AND seen = 0")
+    public List<Book> getUnseenSavedBooks(String userId);
 }
