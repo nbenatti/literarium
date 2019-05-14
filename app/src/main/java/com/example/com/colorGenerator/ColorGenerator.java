ac69fe7affe8c058f25a58ec74d4f62bcfb4e941
FrankDawn <47036534+FrankDawn@users.noreply.github.com>
2019-05-14 16:10:02 +0200

    should have fixed overlapping colors problem

--- app/src/main/java/com/example/com/colorGenerator/ColorGenerator.java
+++ app/src/main/java/com/example/com/colorGenerator/ColorGenerator.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public final class ColorGenerator {
 
-    private final double GOLDEN_RATIO_CONJUGATE = 0.618033988749895;
+    private final double GOLDEN_RATIO = 1.6180339887498948482;
     private final double s, v;
 
     public ColorGenerator(double s, double v) {
@@ -25,15 +25,15 @@ public final class ColorGenerator {
 
         List<String> list = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
-            list.add(getRandomColorString());
+            list.add(getRandomColorString(i));
         }
 
         return list.toArray(new String[0]);
     }
 
-    public String getRandomColorString() {
+    private String getRandomColorString(int i) {
         Random r = new Random();
-        double h = (r.nextDouble() + r.nextInt()*GOLDEN_RATIO_CONJUGATE) % 1;
+        double h = (r.nextDouble() + i / GOLDEN_RATIO) % 1;
         return generateColor(h, v, s).toString();
     }
 
